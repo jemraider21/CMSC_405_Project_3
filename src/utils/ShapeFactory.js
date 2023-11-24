dbdeb8c7132ca8e488af7aeffc1f5551138975be
Jared <jemraider21@gmail.com>
2023-11-24 16:54:48 -0500

    Created the following shapes: Sphere, Cylinder, Cone, Rectangle

--- src/utils/ShapeFactory.js
+++ src/utils/ShapeFactory.js
@@ -4,13 +4,17 @@ import { Colors } from './Colors';
 class ShapeFactory{
     static Cube = new ShapeFactory('Cube');
     static Pyramid = new ShapeFactory('Pyramid');
+    static Sphere = new ShapeFactory('Sphere');
+    static Cylinder = new ShapeFactory('Cylinder');
+    static Cone = new ShapeFactory('Cone');
+    static Rectangle = new ShapeFactory('Rectangle');
 
     constructor(shapeName){
         this.shapeName = shapeName;
     }
 
     static createRandomShape(xPosition, yPosition, zPosition){
-        const shapes = [this.Cube, this.Pyramid];
+        const shapes = [this.Cube, this.Pyramid, this.Sphere, this.Cylinder, this.Cone, this.Rectangle];
         const randomNumber = Math.floor(Math.random() * shapes.length) + 1;
         const randomShape = shapes[randomNumber - 1];
         const randomColor = Colors.createRandomColor();
@@ -25,6 +29,14 @@ class ShapeFactory{
                 return this.#createCube(xPosition, yPosition, zPosition, colorRepresentation);
             case 'Pyramid':
                 return this.#createPyramid(xPosition, yPosition, zPosition, colorRepresentation);
+            case 'Sphere':
+                return this.#createSphere(xPosition, yPosition, zPosition, colorRepresentation);
+            case 'Cylinder':
+                return this.#createCylinder(xPosition, yPosition, zPosition, colorRepresentation);
+            case 'Cone':
+                return this.#createCone(xPosition, yPosition, zPosition, colorRepresentation);
+            case 'Rectangle':
+                return this.#createRectangle(xPosition, yPosition, zPosition, colorRepresentation);
             default:
                 throw new Error('Invalid shape name');
         }
@@ -56,6 +68,62 @@ class ShapeFactory{
         pyramid.position.z = zPosition;
     
         return pyramid;
+    }
+
+    #createSphere(xPosition, yPosition, zPosition, colorRepresentation) {
+        // Create a sphere
+        const sphereGeometry = new THREE.SphereGeometry(2, 32, 32);
+        const sphereMaterial = new THREE.MeshBasicMaterial({ color: colorRepresentation});
+        const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    
+        // Set the sphere in the lower right corner
+        sphere.position.x = xPosition;
+        sphere.position.y = yPosition;
+        sphere.position.z = zPosition;
+    
+        return sphere;
+    }
+
+    #createCylinder(xPosition, yPosition, zPosition, colorRepresentation) {
+        // Create a cylinder
+        const cylinderGeometry = new THREE.CylinderGeometry(2, 2, 4, 32);
+        const cylinderMaterial = new THREE.MeshBasicMaterial({ color: colorRepresentation});
+        const cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+    
+        // Set the cylinder in the lower left corner
+        cylinder.position.x = xPosition;
+        cylinder.position.y = yPosition;
+        cylinder.position.z = zPosition;
+    
+        return cylinder;
+    }
+
+    #createCone(xPosition, yPosition, zPosition, colorRepresentation) {
+        // Create a cone
+        const coneGeometry = new THREE.ConeGeometry(2, 4, 32);
+        const coneMaterial = new THREE.MeshBasicMaterial({ color: colorRepresentation});
+        const cone = new THREE.Mesh(coneGeometry, coneMaterial);
+    
+        // Set the cone in the center
+        cone.position.x = xPosition;
+        cone.position.y = yPosition;
+        cone.position.z = zPosition;
+    
+        return cone;
+    }
+
+    #createRectangle(xPosition, yPosition, zPosition, colorRepresentation) {
+        // Create a rectangle
+        const rectangleGeometry = new THREE.BoxGeometry(2, 4, 2);
+        const rectangleMaterial = new THREE.MeshBasicMaterial({ color: colorRepresentation});
+        const rectangle = new THREE.Mesh(rectangleGeometry, rectangleMaterial);
+    
+        // Set the rectangle in the center
+        rectangle.position.x = xPosition;
+        rectangle.position.y = yPosition;
+        rectangle.position.z = zPosition;
+    
+        return rectangle;
     }
 }
 
