97ae1610f86ae67622cdf16ec8d74fa833f24c7d
jemraider21 <jemraider21@gmail.com>
2023-11-25 13:04:06 -0500

    Added camera controls and a toggle to make a selected shape visible or in

--- src/main/main.js
+++ src/main/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { ShapeFactory } from './utils/ShapeFactory';
 
 // Create a scene
@@ -13,34 +14,75 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const shape1 = ShapeFactory.createRandomShape(-15, 7);
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.set(0, 20, 20);
+controls.update();
+
+// Create a map to hold all of the shapes for later actions
+const shapes = new Map();
+
+const shape1 = ShapeFactory.createRandomShape(-15, 7, "Shape 1");
 scene.add(shape1);
+shapes.set(shape1.name, shape1);
 
-const shape2 = ShapeFactory.createRandomShape(0, 7);
+const shape2 = ShapeFactory.createRandomShape(0, 7, "Shape 2");
 scene.add(shape2);
+shapes.set(shape2.name, shape2);
 
-const shape3 = ShapeFactory.createRandomShape(15, 7);
+const shape3 = ShapeFactory.createRandomShape(15, 7, "Shape 3");
 scene.add(shape3);
+shapes.set(shape3.name, shape3);
 
-const shape4 = ShapeFactory.createRandomShape(-15, -7);
+const shape4 = ShapeFactory.createRandomShape(-15, -7, "Shape 4");
 scene.add(shape4);
+shapes.set(shape4.name, shape4);
 
-const shape5 = ShapeFactory.createRandomShape(0, -7);
+const shape5 = ShapeFactory.createRandomShape(0, -7, "Shape 5");
 scene.add(shape5);
+shapes.set(shape5.name, shape5);
 
-const shape6 = ShapeFactory.createRandomShape(15, -7);
+const shape6 = ShapeFactory.createRandomShape(15, -7, "Shape 6");
 scene.add(shape6);
-
+shapes.set(shape6.name, shape6);
 
 // Animation loop
 const animate = () => {
   requestAnimationFrame(animate);
-  const shapes = [shape1, shape2, shape3, shape4, shape5, shape6];
   shapes.forEach(shape => {
     shape.rotation.x += 0.01;
     shape.rotation.y += 0.01;
   });
+  controls.update();
   renderer.render(scene, camera);
 };
 
 animate();
+
+const shapeDropdown = document.getElementById('shapeDropdown');
+const toggleButton = document.getElementById('toggleShape');
+
+// Change the text of the toggle button when the shape dropdown changes
+shapeDropdown.addEventListener('change', () => {
+  const selectedShape = shapeDropdown.value;
+  console.log(`Selected shape: ${selectedShape}`);
+  const shape = shapes.get(selectedShape);
+  if(shape.visible){
+    toggleButton.innerText = `Hide ${selectedShape}`;
+  } else {
+    toggleButton.innerText = `Show ${selectedShape}`;
+  }
+});
+
+// Toggle the visibility of the selected shape and change the text of the toggle button
+toggleButton.addEventListener('click', () => {
+  const selectedShape = shapeDropdown.value;
+  const shape = shapes.get(selectedShape);
+  shape.visible = !shape.visible;
+  if(shape.visible){
+    alert(`${shape.name} is now visible`);
+    toggleButton.innerText = `Hide ${selectedShape}`;
+  } else {
+    alert(`${shape.name} is now hidden`);
+    toggleButton.innerText = `Show ${selectedShape}`;
+  }
+});
--- src/main/utils/ShapeFactory.js
+++ src/main/utils/ShapeFactory.js
@@ -14,40 +14,41 @@ import { Colors } from './Colors';
         this.shapeName = shapeName;
     }
 
-    static createRandomShape(xPosition, yPosition){
+    static createRandomShape(xPosition, yPosition, shapeName){
         const shapes = [this.Cube, this.Pyramid, this.Sphere, this.Cylinder, this.Cone, this.Rectangle];
         const randomNumber = Math.floor(Math.random() * shapes.length) + 1;
         const randomShape = shapes[randomNumber - 1];
         const randomColor = Colors.createRandomColor();
 
         console.log(`Creating a ${randomShape.shapeName} with color ${randomColor.colorName}`);
-        return randomShape.createShape(xPosition, yPosition, randomColor.getColorRepresentation());
+        return randomShape.createShape(xPosition, yPosition, randomColor.getColorRepresentation(), shapeName);
     }
 
-    createShape(xPosition, yPosition, colorRepresentation){
+    createShape(xPosition, yPosition, colorRepresentation, shapeName){
         switch(this.shapeName){
             case 'Cube':
-                return this.#createCube(xPosition, yPosition, colorRepresentation);
+                return this.#createCube(xPosition, yPosition, colorRepresentation, shapeName);
             case 'Pyramid':
-                return this.#createPyramid(xPosition, yPosition, colorRepresentation);
+                return this.#createPyramid(xPosition, yPosition, colorRepresentation, shapeName);
             case 'Sphere':
-                return this.#createSphere(xPosition, yPosition, colorRepresentation);
+                return this.#createSphere(xPosition, yPosition, colorRepresentation, shapeName);
             case 'Cylinder':
-                return this.#createCylinder(xPosition, yPosition, colorRepresentation);
+                return this.#createCylinder(xPosition, yPosition, colorRepresentation, shapeName);
             case 'Cone':
-                return this.#createCone(xPosition, yPosition, colorRepresentation);
+                return this.#createCone(xPosition, yPosition, colorRepresentation, shapeName);
             case 'Rectangle':
-                return this.#createRectangle(xPosition, yPosition, colorRepresentation);
+                return this.#createRectangle(xPosition, yPosition, colorRepresentation, shapeName);
             default:
                 throw new Error('Invalid shape name');
         }
     }
 
-    #createCube(xPosition, yPosition, colorRepresentation) {
+    #createCube(xPosition, yPosition, colorRepresentation, shapeName) {
         // Create a cube
         const cubeGeometry = new THREE.BoxGeometry();
         const cubeMaterial = new THREE.MeshBasicMaterial({ color: colorRepresentation});
         const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+        cube.name = shapeName;
     
         // Set the cube in the upper left corner
         cube.position.x = xPosition;
@@ -57,11 +58,12 @@ import { Colors } from './Colors';
         return cube;
     }
 
-    #createPyramid(xPosition, yPosition, colorRepresentation) {
+    #createPyramid(xPosition, yPosition, colorRepresentation, shapeName) {
         // Create a pyramid
         const pyramidGeometry = new THREE.ConeGeometry(2, 4, 4);
         const pyramidMaterial = new THREE.MeshBasicMaterial({ color: colorRepresentation});
         const pyramid = new THREE.Mesh(pyramidGeometry, pyramidMaterial);
+        pyramid.name = shapeName;
     
         // Set the pyramid in the upper right corner
         pyramid.position.x = xPosition;
@@ -71,11 +73,12 @@ import { Colors } from './Colors';
         return pyramid;
     }
 
-    #createSphere(xPosition, yPosition, colorRepresentation) {
+    #createSphere(xPosition, yPosition, colorRepresentation, shapeName) {
         // Create a sphere
         const sphereGeometry = new THREE.SphereGeometry(2, 32, 32);
         const sphereMaterial = new THREE.MeshBasicMaterial({ color: colorRepresentation});
         const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+        sphere.name = shapeName;
     
         // Set the sphere in the lower right corner
         sphere.position.x = xPosition;
@@ -85,11 +88,12 @@ import { Colors } from './Colors';
         return sphere;
     }
 
-    #createCylinder(xPosition, yPosition, colorRepresentation) {
+    #createCylinder(xPosition, yPosition, colorRepresentation, shapeName) {
         // Create a cylinder
         const cylinderGeometry = new THREE.CylinderGeometry(2, 2, 4, 32);
         const cylinderMaterial = new THREE.MeshBasicMaterial({ color: colorRepresentation});
         const cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+        cylinder.name = shapeName;
     
         // Set the cylinder in the lower left corner
         cylinder.position.x = xPosition;
@@ -99,11 +103,12 @@ import { Colors } from './Colors';
         return cylinder;
     }
 
-    #createCone(xPosition, yPosition, colorRepresentation) {
+    #createCone(xPosition, yPosition, colorRepresentation, shapeName) {
         // Create a cone
         const coneGeometry = new THREE.ConeGeometry(2, 4, 32);
         const coneMaterial = new THREE.MeshBasicMaterial({ color: colorRepresentation});
         const cone = new THREE.Mesh(coneGeometry, coneMaterial);
+        cone.name = shapeName;
     
         // Set the cone in the center
         cone.position.x = xPosition;
@@ -113,11 +118,12 @@ import { Colors } from './Colors';
         return cone;
     }
 
-    #createRectangle(xPosition, yPosition, colorRepresentation) {
+    #createRectangle(xPosition, yPosition, colorRepresentation, shapeName) {
         // Create a rectangle
         const rectangleGeometry = new THREE.BoxGeometry(2, 4, 2);
         const rectangleMaterial = new THREE.MeshBasicMaterial({ color: colorRepresentation});
         const rectangle = new THREE.Mesh(rectangleGeometry, rectangleMaterial);
+        rectangle.name = shapeName;
     
         // Set the rectangle in the center
         rectangle.position.x = xPosition;
